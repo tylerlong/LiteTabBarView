64957455f8647fa32c2ddd958711ab715ea5f366
Tyler Long <tyler4long@gmail.com>
2015-11-05 22:59:11 +0800

    Initial Commit

---
+++ LiteTabBarView/LiteTabBarView.h
@@ -0,0 +1,19 @@
+//
+//  LiteTabBarView.h
+//  LiteTabBarView
+//
+//  Created by Tyler Long on 5/11/2015.
+//  Copyright © 2015 Tylingsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LiteTabBarView.
+FOUNDATION_EXPORT double LiteTabBarViewVersionNumber;
+
+//! Project version string for LiteTabBarView.
+FOUNDATION_EXPORT const unsigned char LiteTabBarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiteTabBarView/PublicHeader.h>
+
+
